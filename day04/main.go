02e60ca84228b33c353b584720dce0898c4c25a2
Kevin Ho <kevin.ho01@sap.com>
2025-12-04 09:33:53 -0800

    day04 part1 complete

---
+++ day04/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"aoc-2025/common"
+	"bytes"
+	"fmt"
+	"log"
+)
+
+func buildGrid(data []byte) [][]byte {
+	trimWhiteSpace, _ := bytes.CutSuffix(data, []byte{10})
+	return bytes.Split(trimWhiteSpace, []byte{10})
+}
+
+func isValidRoll(x, y int, grid [][]byte) bool {
+
+	X := len(grid[0])
+	Y := len(grid)
+	deltas := [][2]int{
+		{-1, 1},
+		{0, 1},
+		{1, 1},
+		{-1, 0},
+		{1, 0},
+		{-1, -1},
+		{0, -1},
+		{1, -1},
+	}
+
+	var surrounds int
+	for _, delta := range deltas {
+		newX := x + delta[0]
+		newY := y + delta[1]
+
+		// case: out of bounds
+		if !(0 <= newX && newX < X) || !(0 <= newY && newY < Y) {
+			continue
+		}
+		if grid[newY][newX] == 64 {
+			surrounds++
+		}
+
+	}
+
+	return surrounds < 4
+
+}
+
+func countValidRolls(grid [][]byte) int {
+	// @ = 64
+	var count int
+	var validRolls [][2]int
+	for y, row := range grid {
+		for x := range row {
+			if grid[y][x] == 64 && isValidRoll(x, y, grid) {
+				count++
+				validRolls = append(validRolls, [2]int{x, y})
+			}
+
+		}
+	}
+
+	return count
+}
+
+func main() {
+
+	filePath := "./input.txt"
+	data, err := common.ReadInput(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grid := buildGrid(data)
+
+	ct := countValidRolls(grid)
+
+	fmt.Println(ct)
+
+}
